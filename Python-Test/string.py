d7b721d1c271f489a08ffa7e00fe40f497b18320
Ayan <ayand777@gmail.com>
2021-03-04 12:52:25 +0530

    modified

--- Python-Test/string.py
+++ Python-Test/string.py
@@ -11,4 +11,21 @@ word = "is"
 if word in SearchMe:
     print("Pass")
 
+s = "0123456789"
+
+print("Even:", s[0::2])
+print("Odd", s[1::2])
+
+i = 0
+print("Even")
+while i < len(s):
+    print(s[i])
+    i = i + 2
+
+i = 1
+print("Even")
+while i < len(s):
+    print(s[i])
+    i = i + 2
+
 
